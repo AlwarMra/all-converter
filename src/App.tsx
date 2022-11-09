1f682015ee3836606ae443caf04a8e08ea2f581b
alwar <alwaromoreno@gmail.com>
2022-11-09 20:44:41 +0100

    React router. Home and Colors route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Colors from './pages/Colors'
+import Home from './pages/Home'
 import './App.css'
 
-function App() {
-  return <div></div>
+function App(): JSX.Element {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/convert/colors' element={<Colors />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
-   
